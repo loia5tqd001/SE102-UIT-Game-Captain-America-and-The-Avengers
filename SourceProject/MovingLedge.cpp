eb2d69462fb7673f903f3c5a155288296378bddb
quanghuyUIT <quanghuy1998kh@gmail.com>
2019-07-30 18:36:54 +0700

    add

--- SourceProject/MovingLedge.cpp
+++ SourceProject/MovingLedge.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "MovingLedge.h"
+#define PI 3.14159265
 
 MovingLedge::MovingLedge(Vector2 pos, Behaviors behavior) :
 	VisibleObject(State::MovingLedge_Stop, pos),
@@ -11,7 +12,12 @@ MovingLedge::MovingLedge(Vector2 pos, Behaviors behavior) :
 
 void MovingLedge::OnCircleMoving(float dt)
 {
-
+	//eclipse(580,612) 30 52
+	static int counter = 0;
+	static float x = 52*cos(counter / 10 * dt);
+	static float y = 30*cos(counter / 10 * dt - PI / 4);
+	pos.x = 580 + x;
+	pos.y = 612 + y;
 }
 
 void MovingLedge::OnDiagonalMoving(float dt)
